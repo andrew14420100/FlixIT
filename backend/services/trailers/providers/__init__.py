c661c4b48000150d7e643d77682350ac6907e5f3
andrew14420100 <151857640+andrew14420100@users.noreply.github.com>
2026-09-18 20:17:53 +0200

    Add trailer provider package

---
+++ backend/services/trailers/providers/__init__.py
@@ -0,0 +1,11 @@
+from .apple_tv import AppleTVTrailerProvider
+from .imdb import IMDbTrailerProvider
+from .prime_video import PrimeVideoTrailerProvider
+from .netflix import NetflixTrailerProvider
+
+__all__ = [
+    "AppleTVTrailerProvider",
+    "IMDbTrailerProvider",
+    "PrimeVideoTrailerProvider",
+    "NetflixTrailerProvider",
+]
