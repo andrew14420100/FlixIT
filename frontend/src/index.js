75b2e9bc2f7318702a329e8ac29f6e73f99eb66a
andrew14420100 <151857640+andrew14420100@users.noreply.github.com>
2026-09-25 08:51:15 +0200

    feat(player): enable maximum-quality HLS startup globally

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./CustomClassNameSetup";
+import "./playerQualityBootstrap";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
